1e7d945a0483843fd88d2ddbddf552f874d88c12
João Pedro <55983396+Pedro-Joao@users.noreply.github.com>
2026-02-04 03:28:00 +0000

    add `database.js`and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, responde) {
-  responde.status(200).send("teste");
+import database from "../../../../infra/database.js";
+
+async function status(request, responde) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
+  responde.status(200).json({ chve: "teste" });
 }
 
 export default status;
